92f271dee349a943d667d9078e89320139edeebe
samponting <samuel.ponting@pmb.ox.ac.uk>
2025-05-02 15:40:10 +0100

    correct normalisation

--- SHdemo.m
+++ SHdemo.m
@@ -3,22 +3,33 @@ maxHarmOrders = 2;
 
 %Load image. 'im' should ultimately be a a m*n*1 image - whether this is a
 %luminance image, rgb2gray, or by selecting one channel.
-im = exrread('jovi-19-03-06_s03.exr');im = im2double(im);%im = im./max(im(:));
-im = 0.2126*im(:,:,1) + 0.7152*im(:,:,2) + 0.0722*im(:,:,3);
+files = dir([pwd,'/*.hdr']);
 
-%Decompose spherical harmonics. c - coefficients, l-degrees, m-orders.
-[cComplex,l,m] = decomposeSphericalHarmonics(im,maxHarmOrders);
+for imageNum = 1:length(files)
+    
+    %Load Image. Must be Normalised for diffuseness to be correct.
+    im = HDRRead(files(imageNum).name);im = im./max(im(:));
+    im = rgb2gray(im);
 
-%Convert complex SH coefficients to real SH coefficients.
-cReal = complex2realSH(cComplex,l,m);
-
-%Calculate theta and phi (elevation and phi) of the first degree. Convert
-%angles to image positions.
-[elev,phi,imE,imP] = SHcoordinates2imageLocation(cReal,im);
-
-%Get power of each degree component.
-amps = getDegreeAmplitudes(cReal,l);
-
-%Calculate D(Xia) diffuseness metric.
-dXia = calculateDiffuseness(amps);
-
+    
+    %Decompose spherical harmonics. c - coefficients, l-degrees, m-orders.
+    [cComplex,l,m] = decomposeSphericalHarmonics(im,maxHarmOrders);
+    
+    %Convert complex SH coefficients to real SH coefficients.
+    cReal = complex2realSH(cComplex,l,m);
+    
+    %Calculate theta and phi (elevation and phi) of the first degree. Convert
+    %angles to image positions.
+    [elev,phi,imE,imP] = SHcoordinates2imageLocation(cReal,im);
+    
+    %Get power of each degree component.
+    amps = getDegreeAmplitudes(cReal,l);
+    
+    %Calculate D(Xia) diffuseness metric.
+    dXia = calculateDiffuseness(amps);
+    
+    subplot(2,3,imageNum)
+    imshow(im);hold on
+    scatter(imP,imE,200,'+','Color',[0 0 0]);
+    title(sprintf('dXia: %s',num2str(dXia)));
+end
